83d3b346d8fd5053c699afc4856d96f980937f18
Ethan Evans <ethan.evans8965@gmail.com>
2025-05-24 23:09:59 -0400

    chore: finalize working Husky, lint-staged, ESLint, Prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+}; 
